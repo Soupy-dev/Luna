5061c3b1906461326f3ec05018aa08d65e5c487e
Soupy-dev <04nihad17@gmail.com>
2026-01-08 15:31:12 -0500

    Fixes to service sheet

--- Luna/Views/View Elements/MediaInfo/Details/Shows-Details.swift
+++ Luna/Views/View Elements/MediaInfo/Details/Shows-Details.swift
@@ -380,7 +380,7 @@ struct TVShowSeasonsSection: View {
                     await MainActor.run {
                         // Update current season title for anime
                         if let seasonTitle = animeSeasonTitles?[season.seasonNumber] {
-                            self.currentSeasonTitle = seasonTitle
+                            currentSeasonTitle.wrappedValue = seasonTitle
                         }
                         self.seasonDetail = detail
                         self.isLoadingSeason = false
@@ -392,7 +392,7 @@ struct TVShowSeasonsSection: View {
                     // For regular TV shows, fetch from TMDB
                     let detail = try await tmdbService.getSeasonDetails(tvShowId: tvShowId, seasonNumber: season.seasonNumber)
                     await MainActor.run {
-                        self.currentSeasonTitle = nil
+                        currentSeasonTitle.wrappedValue = nil
                         self.seasonDetail = detail
                         self.isLoadingSeason = false
                         if let firstEpisode = detail.episodes.first {
